74d892aa13514696e07c451c5851fa3d59ce0718
kendalljdv <119521302+kendalljdv@users.noreply.github.com>
2022-12-07 15:32:03 -0600

    codigo

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html'
+})
+export class AgregarComponent {
+  @Input () 
+  listaPersonajes: Personaje[]=[];
+  
+  
+  nuevo:Personaje ={
+    nombre:'',
+    poder:0
+  }
+
+  
+  agregar (){
+    if(this.nuevo.nombre.trim().length===0){
+      return;
+    }
+    console.log (this.nuevo);
+
+    this.listaPersonajes.push(this.nuevo);
+    this.nuevo= {
+      nombre:'',
+      poder:0
+    }
+  }
+}
